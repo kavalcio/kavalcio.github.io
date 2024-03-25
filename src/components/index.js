70f58b146908cdae012c71c4edeb5534f0bd9d42
kavalcio <sarpkavalcioglu@gmail.com>
2024-03-25 16:57:04 -0400

    Fix import

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as ProjectTile } from './ProjectTile';
 export { default as Pill } from './Pill';
 export { default as Header } from './Header';
 export { default as ContactForm } from './ContactForm';
+export { default as ContactIcons } from './ContactIcons';

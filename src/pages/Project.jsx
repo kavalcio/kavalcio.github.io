74d1efef05355ea27f0d0e19600a2751c7a9c3d0
kavalcio <sarpkavalcioglu@gmail.com>
2024-03-27 23:28:19 -0400

    Add comment

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+// TODO: do i need this page? delete if not
 const Project = () => {
   const params = useParams();
   return <Box>Project {params.projectId}</Box>;
